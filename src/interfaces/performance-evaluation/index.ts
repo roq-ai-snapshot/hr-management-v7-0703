5c4c715ed9c6a048405c29e367fc4506738990ae
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-08-03 13:28:05 +0000

    Initial commit

---
+++ src/interfaces/performance-evaluation/index.ts
@@ -0,0 +1,21 @@
+import { UserInterface } from 'interfaces/user';
+import { GetQueryInterface } from 'interfaces';
+
+export interface PerformanceEvaluationInterface {
+  id?: string;
+  evaluation_date: any;
+  evaluation_score: number;
+  user_id?: string;
+  created_at?: any;
+  updated_at?: any;
+  comment?: string;
+
+  user?: UserInterface;
+  _count?: {};
+}
+
+export interface PerformanceEvaluationGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  user_id?: string;
+  comment?: string;
+}
---
+++ src/validationSchema/performance-evaluations/index.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+export const performanceEvaluationValidationSchema = yup.object().shape({
+  evaluation_date: yup.date().required(),
+  evaluation_score: yup.number().integer().required(),
+  comment: yup.string().nullable(),
+  user_id: yup.string().nullable(),
+});
